ad319c26db329d782aab9340269e4b4a76b3486d
TamasRichter <rtomilobil@gmail.com>
2018-04-08 16:51:58 +0200

    add popup for log reason

---
+++ mnbAddIn/popup/PopUp.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace popup
+{
+    /// <summary>
+    /// Interaction logic for PopUp.xaml
+    /// </summary>
+    public partial class PopUp : Window
+    {
+        public string NewReason { get; private set; }
+        public bool IsChanged { get; private set; }
+        public PopUp(string userName, string timeStamp, string currentReason)
+        {
+            InitializeComponent();
+            UserBox.Text = userName;
+            DateBox.Text = timeStamp;
+            ReasonBox.Text = currentReason;
+            Closing += PopUp_Closing;
+            ShowDialog();
+        }
+
+        private void PopUp_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!NewReason.Equals(ReasonBox.Text))
+            {
+                var box = MessageBox.Show("There are unsaved changes, do you want to save?", 
+                    "Warning!", MessageBoxButton.YesNo);
+                if(box == MessageBoxResult.Yes)
+                {
+                    Update();
+                }
+            }
+        }
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+        private void Update()
+        {
+            NewReason = ReasonBox.Text;
+            IsChanged = true;
+        }
+    }
+}
